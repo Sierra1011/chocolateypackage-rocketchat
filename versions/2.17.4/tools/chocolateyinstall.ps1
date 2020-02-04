b6196e1e1b3c09d18d92c7fa5f896df229f9d019
Simeon Castle <Simeon.Castle@smartodds.co.uk>
2020-02-04 10:40:10 +0000

    Updated 2.17.4

--- versions/2.17.4/tools/chocolateyinstall.ps1
+++ versions/2.17.4/tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 ﻿$ErrorActionPreference = 'Stop';
-Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S /allusers' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.17.2/rocketchat-setup-2.17.2.exe' -checksum '1ae38e7ec2219fb191b4a63c85ff5642d99648fdf63238faf101910cadb777c7' -checksumType 'sha256'
+Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S /allusers' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.17.4/rocketchat-setup-2.17.4.exe' -checksum '15A1E5C3D4E4FA1A88C97C3B6ABDFF540FF4B84CFFAA5F00FEACC748DF3ADCF9' -checksumType 'sha256'
