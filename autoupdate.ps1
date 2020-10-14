d2885a193a94c1ca7a246d1a9edbec703d31d61b
Sierra1011 <43705549+Sierra1011@users.noreply.github.com>
2020-10-14 13:46:47 +0100

    Almost

--- autoupdate.ps1
+++ autoupdate.ps1
@@ -3,6 +3,13 @@ $VERSION = "3.0.3" # Get-Content? Scrape page?
 $TEMPDIR = ".\versions\template\"
 $WORKDIR = ".\versions\$VERSION\"
 
+# Get the Hash from the download
+$File = "C:\Temp\rocketchat-setup-$VERSION.exe"
+$FileURL = "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/$($VERSION)/rocketchat-setup-$($VERSION).exe"
+( New-Object System.Net.WebClient ).DownloadFile($FileURL,$File)
+$Hash = ( Get-FileHash $File -Algorithm SHA256 ).Hash
+Remove-Item $File
+
 # Make version folder
 New-Item -Path $WORKDIR -ItemType Directory
 New-Item -Path "$WORKDIR\tools" -ItemType Directory
@@ -19,6 +26,3 @@ $Content = Get-ChildItem -Path "$TEMPDIR\tools\chocolateyinstall.ps1" | Get-Cont
 $Content = ( $Content -Replace '#VERSION#',$VERSION )
 $Content -Replace '#HASH#',$HASH | Out-File "$WORKDIR\tools\chocolateyinstall.ps1"
 
-
-$FileURL = "https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/$($VERSION)/rocketchat-setup-$($VERSION).exe"
-$Hash = ( Invoke-WebRequest $FileURL | Get-FileHash $File -Algorithm SHA256 ).Hash
