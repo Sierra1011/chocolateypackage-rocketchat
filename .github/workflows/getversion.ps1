46e1c90810d62dd162d8b9b9534e0c19a33b6812
Sierra1011 <43705549+Sierra1011@users.noreply.github.com>
2022-04-20 11:15:45 +0100

    update

--- .github/workflows/getversion.ps1
+++
@@ -1,3 +0,0 @@
-$Headers = @{'Accept'='application/vnd.github.v3+json'}
-$Response = ( Invoke-WebRequest "https://api.github.com/repos/RocketChat/Rocket.Chat.Electron/releases/latest" -Method GET -Headers $Headers) | ConvertFrom-Json
-return $Response.tag_name
