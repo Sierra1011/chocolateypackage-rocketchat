3a9f3d4123e1cee1636edfcfcbe27705ae0e864d
Sierra1011 <Sierra1011@users.noreply.github.com>
2023-10-03 17:03:32 +0000

    Updated to version 3.9.9
    Files changed:
    M	VERSION
    M	rocketchat.nuspec
    M	tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 $ErrorActionPreference = 'Stop';
-Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S /allusers' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/3.9.8/rocketchat-3.9.8-win-x64.exe' -checksum 'e822114c7feb1a7bf40ea95f271a4821bf3e18ea62182d8a6ed9518fc3676dc1' -checksumType 'sha256'
+Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S /allusers' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/3.9.9/rocketchat-3.9.9-win-x64.exe' -checksum 'f69a96771f271a5ca84a619ad7dbe76fb0001d121d1cbfe61bc43e47e3b3a539' -checksumType 'sha256'
