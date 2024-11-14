53a4212a0eefd845546fa5ee02ae73948899bb49
Sierra1011 <Sierra1011@users.noreply.github.com>
2024-11-14 09:06:04 +0000

    Updated to version 4.1.2
    Files changed:
    M	VERSION
    M	rocketchat.nuspec
    M	tools/chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,2 +1,2 @@
 $ErrorActionPreference = 'Stop';
-Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S /allusers' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/4.1.1/rocketchat-4.1.1-win-x64.exe' -checksum 'f0252926f3f3f83db5ee9faf684ff8cfce2f936fbaea6cef83f9d9b990bc5786' -checksumType 'sha256'
+Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S /allusers' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/4.1.2/rocketchat-4.1.2-win-x64.exe' -checksum 'f3e2cbd67b4ee128342928731988f3871663a71fa7c4a1c82633a427430f7dc7' -checksumType 'sha256'
